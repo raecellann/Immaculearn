e76c4dd01bbe11af2abd95887128be092da9f604
zjdelossantos <zeldrickjoaquin@gmail.com>
2026-01-07 15:57:19 +0800

    small changes

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -7,12 +7,8 @@ import MainLoading from "./LoadingComponents/mainLoading";
 const ProtectedRoute = ({ children }) => {
   const { isLoading, isAuthenticated } = useUser();
 
-  console.log(isLoading, isAuthenticated)
-
   // No need for separate auth check here since UserProvider already does it
   // The UserProvider will have already checked auth on mount
-
-  // console.log(isLoading, isAuthenticated)
 
   if (isLoading) return <div className="flex h-screen justify-center items-center"><MainLoading /></div>;
   // if (!isAuthenticated) return <Navigate to="/login" replace />;
